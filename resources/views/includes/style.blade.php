b14ee8a53c1955255700e51ed713f1ad29d1bc5f
dicky <dickyard1300@gmail.com>
2021-01-09 22:20:47 +0700

    Adding Files

---
+++ resources/views/includes/style.blade.php
@@ -0,0 +1,13 @@
+<link
+    rel="stylesheet"
+    href=" {{ url('frontend/libraries/bootstrap/css/bootstrap.css') }} "
+/>
+<link rel="stylesheet" href="{{ url('frontend/styles/main.css') }}" />
+<link
+    href="https://fonts.googleapis.com/css?family=Assistant:200,400,700&&display=swap"
+    rel="stylesheet"
+/>
+<link
+    href="https://fonts.googleapis.com/css?family=Playfair+Display:400,700&display=swap"
+    rel="stylesheet"
+/>
